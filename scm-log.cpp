7de889e2671bba2f258ef9a6bf6f4c2736a97de5
Robert Kooima <robert.kooima@gmail.com>
2012-12-05 23:34:25 +0000

    Oops, forgot to include log module.
    git-svn-id: https://s.snth.net/svn/rlk/scm@3591 dcf3a027-e0e4-0310-89c9-ab9954db6d00

---
+++ scm-log.cpp
@@ -0,0 +1,31 @@
+// Copyright (C) 2011-2012 Robert Kooima
+//
+// LIBSCM is free software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITH-
+// OUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+
+#include <cstdarg>
+#include <cstdio>
+
+//------------------------------------------------------------------------------
+
+void scm_log(const char *fmt, ...)
+{
+    flockfile(stderr);
+    {
+        va_list  ap;
+        va_start(ap, fmt);
+        vfprintf(stderr, fmt, ap);
+         fprintf(stderr, "\n");
+        va_end  (ap);
+    }
+    funlockfile(stderr);
+}
+
+//------------------------------------------------------------------------------
